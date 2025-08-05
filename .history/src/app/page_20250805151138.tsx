5e4a9694e6df30ad51824d4bbaed504a11fe7ecc
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2025-08-05 15:14:49 -0300

    Inciando Projeto

---
+++ .history/src/app/page_20250805151138.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main>
+            <h1 className="text-4xl font-bold text-orange-600">jk-lanches</h1>
+        </main>
+    )
+}
---
+++ .history/src/app/page_20250805151221.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main className="flex min-h-screen items-center justify-center bg-orange-100">
+            <h1 className="text-4xl font-bold text-orange-600">jk-lanches</h1>
+        </main>
+    )
+}
